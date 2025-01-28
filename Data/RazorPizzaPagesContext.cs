91758a11d77c1b9db5abbed6a0670a4101716741
NicholasR512 <150851357+NicholasR512@users.noreply.github.com>
2025-01-28 17:55:46 -0500

    Add files via upload

---
+++ Data/RazorPizzaPagesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesPizza.Models;
+
+namespace RazorPizzaPages.Data
+{
+    public class RazorPizzaPagesContext : DbContext
+    {
+        public RazorPizzaPagesContext (DbContextOptions<RazorPizzaPagesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesPizza.Models.Pizza> Pizza { get; set; } = default!;
+    }
+}
